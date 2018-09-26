0d6c95c0e20df5502d0e9457f27812d2a848523b
IS Choi <insil321@hotmail.com>
2018-09-26 02:32:59 +0000

    automatically backed up by learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,39 @@
-def addition(num1, num2)
+def addition(num1,num2)
+  num1 + num2
+  puts num1+num2
 end
+addition (num1,num2)
 
-def subtraction(num1, num2)
+def subtraction(num1,num2)
+  num1-num2
+  puts num1-num2
 end
+subtraction(num1,num2)
 
-def division(num1, num2)
+def division(num1,num2)
+  num1/num2
+  puts num1/num2
 end
+division(num1,num2)
 
-def multiplication(num1, num2)
+def multiplication(num1,num2)
+  num1*num2
+  puts num1*num2
 end
+multiplication(num1,num2)
 
-def modulo(num1, num2)
+def modulo(num1,num2)
+  num1%num2
+  puts num1%num2
 end
+modulo(num1,num2)
 
 def square_root(num)
+  puts Math.sqrt(num)
 end
+square_root(num)
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  puts num1 +((num2*num3)/ num4)
 end
+order_of_operation(num1, num2, num3, num4)
